7d9b77c72006704b7c086ec8c4e71cf01442e1ac
Lukas Alber <36759141+luksen99@users.noreply.github.com>
2018-06-29 19:23:37 +0200

    Update AnimationAnaNumComp.py

--- Code/AnimationAnaNumComp.py
+++ Code/AnimationAnaNumComp.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Jun 26 12:32:41 2018
 This code is dependent on the Program: DirichletBoundaryConditions
@@ -10,10 +9,8 @@ from matplotlib.animation import FuncAnimation
 from DiffusionEquationBSplines import t,xfine
 
 phi = np.loadtxt('phi1')
-#phi = np.loadtxt('phipulse')
 L = xfine[-1]
 omega = (np.pi/L)
-
 
 fig, ax = plt.subplots()
 ln, = ax.plot([], [], 'r', animated=True,label='numeric')
@@ -24,18 +21,13 @@ def init():
     ax.set_ylim(0, 2) #maximum y value the animation can have
     return ln, ln2,
 
-
 def update(frame):
-    
     ln2.set_data(xfine,np.sin(omega*xfine)*np.exp(-omega**2*t[frame]))
     ln.set_data(xfine,phi[frame])
     return ln, ln2,
-
 
 ani = FuncAnimation(fig, update,init_func=init, blit=True)
 plt.xlabel('Depth of Material')
 plt.ylabel('Temperature')
 plt.legend(loc='best')
 plt.show()
-
-#
